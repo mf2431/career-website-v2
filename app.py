89c5eb186edc1fdc004826ab52af5fe890e6826c
mf2431 <139679097+mf2431@users.noreply.github.com>
2023-07-16 13:10:30 +0000

    job listing

--- app.py
+++ app.py
@@ -2,9 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id': 1,
+    'title': 'Manager',
+    'location': 'London',
+    'salary': '$40k'
+  },
+  
+   {'id': 2,
+    'title': 'Receptionist',
+    'location': 'Hull',
+   },
+  
+   {'id': 3,
+    'title': 'Warehouse worker',
+    'location': 'Manchester',
+    'salary': '$30k'
+   }
+  
+]
+
 @app.route("/")
 def activate_website():
-    return render_template('home.html')
+    return render_template('home.html',
+                           listing=JOBS,
+                          )
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
